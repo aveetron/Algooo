5378ff32c667783f5fc4008a9104a5f87458becf
avee chakraborty <aveechakra@gmail.com>
2018-12-15 04:31:41 +0600

    Update binary-search.py

--- Searching/binary-search.py
+++ Searching/binary-search.py
@@ -13,6 +13,7 @@ def binary_search(number_list, val):
 
 
 if __name__ == "__main__":
+    #the list must need to be sorted, if not then sort it : number_list.sort()
     number_list = [1,2,3,4,5,6,7,8,9,10]
     result = binary_search(number_list, 1000)
     print(result)
